ebf7bd0900eebceba95c8f547f22aef2c5f02970
Fidel <adarfidel8@gmail.com>
2023-09-25 03:27:06 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memeset - Entry point
+ * @s: pointed destination
+ * @b: constant byte
+ * @n: bytes
+ * Return: Always 0 (Success)
+ */
+char *_memeset(char *s, char b, unsigned int n)
+{
+	 int i = 0
+
+	for (; n > 0; i++)
+	{
+		s[i] = b;
+		n ==;
+	}
+	return (s);
